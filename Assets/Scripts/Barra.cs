30eae48941a1f481936edc4fb72353c0118d0b62
Brayan Gutiérrez <br.desarrollador@gmail.com>
2022-07-16 09:51:14 -0500

    Final Script Barra

--- Assets/Scripts/Barra.cs
+++ Assets/Scripts/Barra.cs
@@ -6,10 +6,17 @@ public class Barra : MonoBehaviour
 {
     public float velocidad = 20.0f;
 
+    Vector3 posicionInicial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicionInicial = transform.position;
+    }
+
+    public void  Reset() 
+    {
+        transform.position = posicionInicial;    
     }
 
     // Update is called once per frame
@@ -17,9 +24,11 @@ public class Barra : MonoBehaviour
     {
        float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
        float posX = transform.position.x + (tecladoHorizontal * velocidad * Time.deltaTime); 
+       /* Estas lineas hacen los mismo que Mathf.Clamp y sustituye el posX  
        if(posX>8) posX=8;
        if(posX<-8) posX=-8;
-       transform.position = new Vector3(/*Mathf.Clamp(posX, -8, 8),*/posX, transform.position.y, transform.position.z);
+       */
+       transform.position = new Vector3(Mathf.Clamp(posX, -14, 14),/*posX*/ transform.position.y, transform.position.z);
        
     }
 }
