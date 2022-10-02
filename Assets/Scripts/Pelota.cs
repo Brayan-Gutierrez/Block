831b6df976fc578fe6f4a413a511bd094c7c70b0
Brayan Gutiérrez <br.desarrollador@gmail.com>
2022-10-02 05:41:49 -0500

    Botando pelota
    Se agregaron funcionalidades al script Pelota. ademas se creo un material rebotar

--- Assets/Scripts/Pelota.cs
+++ Assets/Scripts/Pelota.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class Pelota : MonoBehaviour
 {
     public float velocidadInicial = 600f;
-    Rigidbody rig;
+    public Rigidbody rig;
     bool enJuego = false;
+    Vector3 posicionInicial;
+    Transform barra;
 
     void Awake() {
 
         rig = GetComponent<Rigidbody>();
+        barra = GetComponentInParent<Transform>();
         
     }
     // Start is called before the first frame update
-    void Start()
+    void Start()//guarda la posicion incial para resetear
     {
-        
+        posicionInicial = transform.position;
+    }
+
+    public void Reset(){//resetea la posicion inicial del objeto
+        transform.position = posicionInicial;
+        transform.SetParent(barra);
+        enJuego = false;
+        DetenerMovimienton();
+    }
+
+    public void DetenerMovimienton(){
+        rig.isKinematic = true;
+        rig.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -25,6 +40,8 @@ public class Pelota : MonoBehaviour
         if(!enJuego && Input.GetButtonUp("Fire1")){
             enJuego = true;
             transform.SetParent(null);
+            rig.isKinematic = false;
+            rig.AddForce(new Vector3(velocidadInicial,velocidadInicial,0));
         }
     }
 }
